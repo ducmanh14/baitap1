20945e32430dc7c84f2fdecf67f7cba4c77c6b63
manh14 <manh140405@gmail.com>
2025-05-12 10:29:17 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryID/:videoID' element={<Video/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
